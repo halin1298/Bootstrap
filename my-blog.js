3c7d481d8a5c87ac9eb3fdd1027fb5f00cec64d8
Hao Lin <halin15522@gmail.com>
2023-12-02 18:37:57 -0500

    navbar

--- my-blog.js
+++ my-blog.js
@@ -1,5 +1,5 @@
-const h1 = document.querySelector('h1')
-let alertClick = () => {
-    alert('h1 clicked')
-}
-h1.addEventListener('click',alertClick)
+// const h1 = document.querySelector('h1')
+// let alertClick = () => {
+//     alert('h1 clicked')
+// }
+// h1.addEventListener('click',alertClick)
